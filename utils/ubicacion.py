d6105ac92213bc2246b6a1e31406f4d1605fe88e
solidsnk86 <calcagni.gabriel86@gmail.com>
2025-06-29 19:11:17 -0300

    manejando valores no definidos o null

--- utils/ubicacion.py
+++ utils/ubicacion.py
@@ -3,9 +3,11 @@ import requests
 def obtener_ubicacion():
     try:
         respuesta = requests.get("https://solid-geolocation.vercel.app/location")
+        if not respuesta.ok:
+            raise ValueError("No se obtuvo respuesta de la api")
         data = respuesta.json()
-        ciudad = data["city"]["name"]
-        pais = data["country"]["name"]
+        ciudad = data["city"]["name"] or "No disponible"
+        pais = data["country"]["name"] or "No disponible"
         return ciudad, pais
     except Exception as e:
-        print(f"Ocurrió un error al relizar el fetch de datos {e}")
+        print(f"Ocurrió un error: {e}")
